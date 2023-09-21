f5c571a169421710a37fa70c3fb6542d7da6c850
Walter_van_der_Broek <wbvanderbroek@st.noorderpoort.nl>
2023-09-21 09:44:01 +0200

    building scripts

---
+++ RTS Project/Assets/Scripts/MineBuilding.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineBuilding : BuildingBase
+{
+    public MineBuilding()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
