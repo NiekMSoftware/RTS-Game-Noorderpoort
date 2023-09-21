067165e8b7a7a3505b809077d77361af01ea3c7e
Jan-Jaap <113519516+SpherosGames@users.noreply.github.com>
2023-09-21 17:30:42 +0200

    Working on grid implementation of building

--- RTS Project/Assets/Scripts/BuildingManager.cs
+++ RTS Project/Assets/Scripts/BuildingManager.cs
@@ -2,44 +2,55 @@ using UnityEngine;
 
 public class BuildingManager : MonoBehaviour
 {
-    public GameObject[] objects;
+    [SerializeField] PlaceableObject[] objects;
     private GameObject pendingObject;
+    private int currentIndex = -1;
 
     private Vector3 pos;
 
     private RaycastHit hit;
     [SerializeField] private LayerMask layerMask;
 
+    [System.Serializable]
+    class PlaceableObject
+    {
+        public GameObject model;
+        public float yHeight;
+    }
 
     void Update()
     {
         if (pendingObject != null)
         {
             pendingObject.transform.position = pos;
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0) && !GridManager.Instance.GetOccupanyByCollider(pendingObject.GetComponent<Collider>()))
             {
                 PlaceObject();
             }
         }
     }
 
-    public void PlaceObject()
+    private void PlaceObject()
     {
         pendingObject = null;
+        currentIndex = -1;
     }
 
     private void FixedUpdate()
     {
+        if (currentIndex < 0) return;
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit, 1000, layerMask))
         {
-            pos = GridManager.Instance.GetClosestPointOnGrid(hit.point);
+            pos = new Vector3(GridManager.Instance.GetClosestPointOnGrid(hit.point).x, objects[currentIndex].yHeight, GridManager.Instance.GetClosestPointOnGrid(hit.point).z);
         }
     }
 
     public void SelectObject(int index)
     {
-        pendingObject = Instantiate(objects[index], pos, transform.rotation);
+        pendingObject = Instantiate(objects[index].model, pos, transform.rotation);
+        currentIndex = index;
     }
 }
