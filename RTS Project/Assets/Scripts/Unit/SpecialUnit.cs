8297f9b8ac33a9661261045cffe15131c0c8ae0a
Marlies-boop <mkuipers28@st.noorderpoort.nl>
2023-10-05 09:25:46 +0200

    made special unit script again

--- RTS Project/Assets/Scripts/Unit/SpecialUnit.cs
+++ RTS Project/Assets/Scripts/Unit/SpecialUnit.cs
@@ -6,6 +6,21 @@ public class SpecialUnit : Unit
 {
    public SpecialUnit()
     {
-
+        
     }
+    public void InitsStats()
+    {
+        unitHealth = 10;
+    }
+
+    private void Start()
+    {
+        InitsStats();
+    }
+
+    //Special creatures have a lot of health. They randomly spawn (in the fog of war) every X minutes.
+    //They can be captured by “killing” it and feeding it until it’s tamed.
+    //You can then use it as a unit. it can die just like any other unit.
+    //The creature has abilities that are triggered via the UI.
+
 }
