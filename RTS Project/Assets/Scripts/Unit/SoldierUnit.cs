f2766186b2dfc68828d35956994b86cb4c71ba14
Bluecatto <asrampersad@st.noorderpoort.nl>
2023-10-12 12:01:30 +0200

    Testing things in soldierunit
    Not finished

--- RTS Project/Assets/Scripts/Unit/SoldierUnit.cs
+++ RTS Project/Assets/Scripts/Unit/SoldierUnit.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SoldierUnit : Unit
 {
+    public NavMeshAgent navMeshAgent;
+    public float startWaitTime = 4;
+    public float timeToRotate = 2;
+    public float speedWalk = 6;
+    public float speedRun = 9;
+
+    public float viewRadius = 15;
+    public float viewAngle = 90;
+    public LayerMask playerMask;
+    public LayerMask obstacleMask;
+    public float meshResolution = 1f;
+    public int edgeIterations = 4;
+    public float edgeDistance = 0.5f;
+
+    public Transform[] waypoints;
+    int m_CurrentWaypointIndex;
+
+    Vector3 playerLastPosition = Vector3.zero;
+    Vector3 m_PayerPosition;
+
+    
+
     public SoldierUnit()
     {
+       void Start ()
+        {
 
+        }
     }
 
     protected override void Death()
     {
         if (unitHealth <= 0)
         {
-            // Play death anim
+           //Play death anim
         }
     }
 
+   
 }
