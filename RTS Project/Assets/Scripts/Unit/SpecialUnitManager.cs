4a7202cd17dc6bdb7d0fd2030f2d952b7f369ab1
Marlies-boop <mkuipers28@st.noorderpoort.nl>
2023-10-16 15:28:51 +0200

    SpecialUnits Spawning in random places

--- RTS Project/Assets/Scripts/Unit/SpecialUnitManager.cs
+++ RTS Project/Assets/Scripts/Unit/SpecialUnitManager.cs
@@ -30,7 +30,8 @@ public class SpecialUnitManager : MonoBehaviour
     }
     public void SpawnObject()
     {
-        currentUnitObject = Instantiate(specialUnitObject, transform.position, transform.rotation);
+        Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
+        currentUnitObject = Instantiate(specialUnitObject, randomSpawnPosition, Quaternion.identity);
         SpecialUnit currentSpecialUnit = currentUnitObject.GetComponent<SpecialUnit>();
         currentSpecialUnit.refSpecialUnitManager = this;
         UnitList.Add(currentUnitObject);
