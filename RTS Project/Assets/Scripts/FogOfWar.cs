03cd09b0c20b2d16ad7b5b38be9ecb44f799c014
Marlies-boop <mkuipers28@st.noorderpoort.nl>
2023-10-20 11:54:23 +0200

    Fog Of War

--- RTS Project/Assets/Scripts/FogOfWar.cs
+++ RTS Project/Assets/Scripts/FogOfWar.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class FogOfWar : MonoBehaviour
 {
+    public PlayerTestMovement RefPlayerMovement;
     public GameObject FogOfWarPlane;
     public Transform Player;
     public LayerMask FogLayer;
     public float radius = 5f;
-    private float radiusSqr { get { return radius * radius; } }
+    public float MaxAlpha = 1.0f;
+    private float radiusSqr;
 
     private Mesh mesh;
-    private Vector3[] vertices;
-    private Color[] colors;
+    public Vector3[] vertices;
+    public Color[] colors;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,37 +25,50 @@ public class FogOfWar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(RefPlayerMovement.PlayerMoving)
+        {
+            DoRaycast();
+        }
+    }
+
+    public void DoRaycast()
+    {
         Ray r = new Ray(transform.position, Player.position - transform.position);
         RaycastHit hit;
         Debug.Log("RayCast hit");
         
-        if(Physics.Raycast(r, out hit, 1000, FogLayer,QueryTriggerInteraction.Collide))
-        {
-            
+        if(Physics.Raycast(r, out hit, 100, FogLayer,QueryTriggerInteraction.Collide))
+        {          
             for(int i= 0;i<vertices.Length;i++)
             {
-                print("raycast has hit something");
+                print(hit.transform.gameObject);
                 Vector3 v = FogOfWarPlane.transform.TransformPoint(vertices[i]);
                 float dist = Vector3.SqrMagnitude(v - hit.point);
                 if(dist < radiusSqr)
                 {
-                    float alpha = Mathf.Min(colors[i].a, dist / radiusSqr);
+                    float alpha = 0;
                     colors[i].a = alpha;
-                    alpha = 0;
-                    
                 }
+                UpdateColor();
             }
         }
+
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(Player.position, radius);      
     }
 
     void Initialize()
     {
         mesh = FogOfWarPlane.GetComponent<MeshFilter>().mesh;
         vertices = mesh.vertices;
+        radiusSqr = radius * radius;
         colors = new Color[vertices.Length];
         for(int i=0; i<colors.Length; i++)
         {
-            colors[i] = Color.black;
+            colors[i] = Color.black;            
         }
         UpdateColor();
     }
--- RTS Project/Assets/Scripts/Unit/PlayerTestMovement.cs
+++ RTS Project/Assets/Scripts/Unit/PlayerTestMovement.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerTestMovement : MonoBehaviour
 {
     public float speed;
+    public bool PlayerMoving = false;
+    private Transform playerTransform;
+    private Vector3 previousPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTransform = transform;
+        previousPosition = playerTransform.position;
     }
 
     // Update is called once per frame
@@ -21,5 +26,11 @@ public class PlayerTestMovement : MonoBehaviour
         z *= speed * Time.deltaTime;
 
         transform.Translate(x,0,z);
+        if (playerTransform.position != previousPosition)
+        {
+            print("Player is moving");
+            PlayerMoving = true;
+        }   
+        previousPosition = playerTransform.position;
     }
 }
