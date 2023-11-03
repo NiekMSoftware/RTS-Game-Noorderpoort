fb6aebb5359f8bd94f4c9c49f49795e0d129483f
Doggo499 <jbol1@st.noorderpoort.nl>
2023-11-03 15:25:01 +0100

    Working on camera

--- RTS Project/Assets/Scripts/CameraManager.cs
+++ RTS Project/Assets/Scripts/CameraManager.cs
@@ -1,11 +1,27 @@
+using TMPro;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private Transform cameraHolder;
+    [SerializeField] private Transform orientation;
+    [SerializeField] private Transform zoomOrientation;
+    [SerializeField] private TMP_Text debugText;
+
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private Vector2 range;
+
+    [Header("Rotation")]
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float rotationSmoothing;
-    [SerializeField] private Vector2 range;
+    [SerializeField] private Vector2 rotationRangeY;
+
+    [Header("Zoom")]
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float zoomSmoothing;
+    [SerializeField] private Vector2 zoomRange;
 
     private Rigidbody rb;
 
@@ -14,11 +30,16 @@ public class CameraManager : MonoBehaviour
     private Vector2 rotationInput;
     private Vector3 currentRotation;
 
-    private bool isColliding;
+    private float zoomInput;
+    private Vector3 cameraDirection => transform.InverseTransformDirection(cameraHolder.forward);
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        rb.inertiaTensor = Vector3.one;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
@@ -27,25 +48,69 @@ public class CameraManager : MonoBehaviour
 
         CheckBounds();
 
+        RotateOrientation();
+
         Movement();
 
         Rotation();
 
-        //Vector3 zoomDir = new Vector3
+        Zoom();
+    }
+
+    private void RotateOrientation()
+    {
+        orientation.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+    }
+
+    private void Zoom()
+    {
+        if (transform.position.y > zoomRange.y)
+        {
+            zoomInput = Mathf.Clamp(zoomInput, 0, 1);
+        }
+        else if (transform.position.y < zoomRange.x)
+        {
+            zoomInput = Mathf.Clamp(zoomInput, -1, 0);
+        }
+
+        if (zoomInput != 0 && ZoomIsInBounds(cameraHolder.localPosition))
+        {
+            rb.AddForce(Time.deltaTime * zoomInput * zoomSmoothing * zoomOrientation.forward, ForceMode.Force);
+        }
+    }
+
+    private bool ZoomIsInBounds(Vector3 position)
+    {
+        print(position.magnitude);
+        print(position.magnitude > zoomRange.x && position.magnitude < zoomRange.y);
+        return position.magnitude > zoomRange.x && position.magnitude < zoomRange.y;
     }
 
     private void Rotation()
     {
-        currentRotation = Vector3.Lerp(currentRotation, rotationInput, Time.deltaTime * rotationSmoothing);
+        //print(-transform.right);
+        debugText.SetText(transform.up.ToString());
 
-        //transform.rotation = Quaternion.Euler(currentRotation.x, currentRotation.y, 0);
-        rb.AddTorque(transform.up * currentRotation.x * rotationSpeed, ForceMode.Force);
-        rb.AddTorque(-transform.right * currentRotation.y * rotationSpeed, ForceMode.Force);
+        if (transform.up.y <= rotationRangeY.x)
+        {
+            print("too high");
+            //rotationInput.y = Mathf.Clamp(rotationInput.y, -1, 0);
+        }
 
-        currentRotation = Vector3.zero;
-        rotationInput = Vector2.zero;
-        //Vector3 torque = new(currentRotation.x, currentRotation.y, 0);
-        //rb.AddRelativeTorque(torque, ForceMode.VelocityChange);
+        if (transform.up.y >= rotationRangeY.y)
+        {
+            print("too low");
+            //rotationInput.y = Mathf.Clamp(rotationInput.y, 0, 1);
+        }
+
+        //currentRotation = Vector3.Lerp(currentRotation, rotationInput, Time.deltaTime * rotationSmoothing);
+        currentRotation = rotationInput;
+
+        rb.AddRelativeTorque(currentRotation.x * rotationSpeed * transform.up, ForceMode.Force);
+        //rb.AddRelativeTorque(currentRotation.y * rotationSpeed * -transform.right, ForceMode.Force);
+
+        Vector3 eulerAngles = transform.eulerAngles;
+        transform.eulerAngles = new Vector3(eulerAngles.x, eulerAngles.y, 0);
 
         currentRotation = Vector3.zero;
         rotationInput = Vector2.zero;
@@ -62,15 +127,19 @@ public class CameraManager : MonoBehaviour
     private void HandleInput()
     {
         //Movement Input
-        input = new(Input.GetAxisRaw("Horizontal"), Input.mouseScrollDelta.y, Input.GetAxisRaw("Vertical"));
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
 
-        Vector3 right = transform.right * input.x;
-        Vector3 forward = transform.forward * input.z;
+        Vector3 right = orientation.right * x;
+        Vector3 forward = orientation.forward * z;
 
         input = (right + forward).normalized;
 
+        //Zoom Input
+        zoomInput = Input.mouseScrollDelta.y;
+
         //Rotation Input
-        if (!Input.GetMouseButton(2)) return;
+        if (!Input.GetMouseButton(1)) return;
 
         rotationInput += new Vector2(Input.GetAxisRaw("Mouse X") * rotationSpeed, Input.GetAxisRaw("Mouse Y") * rotationSpeed);
     }
@@ -92,15 +161,5 @@ public class CameraManager : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(transform.position, 5f);
         Gizmos.DrawWireCube(Vector3.zero, new Vector3(range.x * 2f, 5f, range.y * 2f));
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        isColliding = true;
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        isColliding = false;
     }
 }
