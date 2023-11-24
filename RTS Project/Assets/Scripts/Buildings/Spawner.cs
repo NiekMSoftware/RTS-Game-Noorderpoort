5d82fb1b9915cb339d8699bf55c64603251af4ae
Bluecatto <asrampersad@st.noorderpoort.nl>
2023-11-24 13:45:36 +0100

    Spawner stuff

--- RTS Project/Assets/Scripts/Buildings/Spawner.cs
+++ RTS Project/Assets/Scripts/Buildings/Spawner.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject cubePrefab;
+    public float spawnInterval = 3f;
+    public int maxCubes = 6; 
+    private float timer = 0f;
+    private int spawnedCubes = 0;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval && spawnedCubes < maxCubes)
         {
             Instantiate(cubePrefab, transform.position, Quaternion.identity);
+
+            timer = 0f;
+
+            spawnedCubes++;
         }
     }
 }
