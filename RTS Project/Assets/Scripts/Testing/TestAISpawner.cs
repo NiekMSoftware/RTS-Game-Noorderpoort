b85fb8329ff2d69371fba6f962e45f34294b86db
Marlies-boop <mkuipers28@st.noorderpoort.nl>
2023-11-27 13:38:07 +0100

    fog of war en Soldier test

--- RTS Project/Assets/Scripts/Testing/TestAISpawner.cs
+++ RTS Project/Assets/Scripts/Testing/TestAISpawner.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class TestAISpawner : MonoBehaviour
 {
     [SerializeField] private GameObject aiToSpawn;
+    [SerializeField] private GameObject SoldierToSpawn;
     [SerializeField] private LayerMask ground;
     [SerializeField] private Terrain terrain;
 
@@ -17,6 +18,18 @@ public class TestAISpawner : MonoBehaviour
                 Vector3 spawnPos = hit.point;
                 spawnPos.y = terrain.SampleHeight(hit.point) + aiToSpawn.transform.localScale.y;
                 GameObject spawnedAI = Instantiate(aiToSpawn, spawnPos, Quaternion.identity);
+                spawnedAI.GetComponent<Unit>().typeUnit = Unit.TypeUnit.Human;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, ground))
+            {
+                Vector3 spawnPos = hit.point;
+                spawnPos.y = terrain.SampleHeight(hit.point) + SoldierToSpawn.transform.localScale.y;
+                GameObject spawnedAI = Instantiate(SoldierToSpawn, spawnPos, Quaternion.identity);
                 spawnedAI.GetComponent<Unit>().typeUnit = Unit.TypeUnit.Human;
             }
         }
