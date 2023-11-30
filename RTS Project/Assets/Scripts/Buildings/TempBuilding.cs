f19c0e5f48fc3b9413907477917df8db65fa3799
Doggy499 <janjaapbol93@gmail.com>
2023-11-30 11:35:16 +0100

    Working on building manager revamp

--- RTS Project/Assets/Scripts/Buildings/TempBuilding.cs
+++ RTS Project/Assets/Scripts/Buildings/TempBuilding.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class TempBuilding : MonoBehaviour
 {
+    private GameObject buildingToSpawn;
+    private float buildTime;
 
+    private void Update()
+    {
+        if (buildingToSpawn == null) return;
+
+        buildTime -= Time.deltaTime;
+
+        if (buildTime < 0)
+        {
+            Build();
+        }
+    }
+
+    public void Setup(GameObject buildingToSpawn, float buildTime)
+    {
+        this.buildingToSpawn = buildingToSpawn;
+        this.buildTime = buildTime;
+    }
+
+    private void Build()
+    {
+        Instantiate(buildingToSpawn, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
 }
