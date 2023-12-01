a163ac470f2317e214ef7e879e0d917c27363bc9
Walter_van_der_Broek <wbvanderbroek@st.noorderpoort.nl>
2023-12-01 15:24:21 +0100

    jj kom

--- RTS Project/Assets/Scripts/Buildings/BuildingManager.cs
+++ RTS Project/Assets/Scripts/Buildings/BuildingManager.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class BuildingManager : MonoBehaviour
+public class BuildingManager : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField] private Building[] buildings;
@@ -287,6 +287,7 @@ public class BuildingManager : MonoBehaviour
         }
 
         ParticleSystem spawnedParticle = Instantiate(buildParticle, pos, Quaternion.identity).GetComponent<ParticleSystem>();
+        //spawnedParticle.GetComponent<NetworkObject>().Spawn(true);
         spawnedParticle.Play();
 
         BuildingBase spawnedBuilding = Instantiate(buildings[currentIndex].building, pendingObject.transform.position, pendingObject.transform.rotation).GetComponent<BuildingBase>();
