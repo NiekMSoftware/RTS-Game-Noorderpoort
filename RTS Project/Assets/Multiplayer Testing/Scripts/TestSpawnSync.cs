6290a0b032def30b5a27397eeae6d52b05ff96f0
Walter_van_der_Broek <wbvanderbroek@st.noorderpoort.nl>
2023-12-01 14:18:42 +0100

    spawntesting

--- RTS Project/Assets/Multiplayer Testing/Scripts/TestSpawnSync.cs
+++ RTS Project/Assets/Multiplayer Testing/Scripts/TestSpawnSync.cs
@@ -1,12 +1,10 @@
 using Unity.Netcode;
+using Unity.Services.Authentication;
 using UnityEngine;
 
 public class TestSpawnSync : NetworkBehaviour
 {
     [SerializeField] private GameObject spawnedObjectPrefab;
-    private GameObject spawnedObjectTransform;
-
-
 
     void Update()
     {
@@ -21,52 +19,12 @@ public class TestSpawnSync : NetworkBehaviour
     {
         Color objectColor = Color.blue;
 
-        spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
+        GameObject spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
         spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
         spawnedObjectTransform.GetComponent<Renderer>().material.color = objectColor;
-        SpawnObjectPrintClientRpc();
-    }
+        print(NetworkObjectId);
 
-    [ClientRpc]
-    void SpawnObjectPrintClientRpc()
-    {
-        print(spawnedObjectTransform.GetComponent<NetworkObject>().IsOwner);
-
-    }
-
-
-    // Network variable to synchronize team ID
-    public NetworkVariable<int> teamID = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-
-    // Other player-related variables and methods...
-
-    private void Start()
-    {
-        print(IsOwner);
-        this.OnGainedOwnership();
-        print(IsOwner);
-        // Set initial team ID when the player spawns
-        if (IsLocalPlayer)
-        {
-            // You might want to implement a team selection menu for players
-            // For simplicity, let's assume the player starts with team 1
-            CmdChangeTeamServerRpc(1);
-        }
-    }
-
-    [ServerRpc]
-    private void CmdChangeTeamServerRpc(int newTeamID)
-    {
-        teamID.Value = newTeamID;
-        RpcUpdateTeamOnClientsClientRpc(newTeamID);
-    }
-
-    [ClientRpc]
-    private void RpcUpdateTeamOnClientsClientRpc(int newTeamID)
-    {
-        // This method is called on all clients when the team ID changes
-        teamID.Value = newTeamID;
-
-        // Update visuals, materials, or any other client-side changes related to team
+        //print(spawnedObjectTransform.GetComponent<NetworkObject>().ChangeOwnership(this.id))
+        print(spawnedObjectTransform.GetComponent<NetworkObject>().NetworkObjectId);
     }
 }
