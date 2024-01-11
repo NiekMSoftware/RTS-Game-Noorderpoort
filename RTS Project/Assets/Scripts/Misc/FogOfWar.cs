5f18c20ec523132157d763dc87a4a9761c8733a0
Niek Melet <niek+github@meletstudios.com>
2024-01-11 18:01:23 +0100

    Update FOW
    Made sure all the locations of each of the Soldiers' location is being updated accordingly, what is left now is to send rays to every soldier

--- RTS Project/Assets/Scripts/Misc/FogOfWar.cs
+++ RTS Project/Assets/Scripts/Misc/FogOfWar.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +13,54 @@ public class FogOfWar : MonoBehaviour
     public List<SoldierUnit> soldiers;
     public List<Unit> enemyUnits;
 
+    [Space]
+    
+    public LayerMask soldierLayer;
+
     [Header("Fog of War Properties")]
     [SerializeField] private Transform fogTransform;
+
+    void Update()
+    {
+        // Gather all local soldiers
+        UpdateSoldierList();
+
+        // Only start routine once there are units in the list
+        if (soldiers.Count > 0)
+            StartCoroutine(nameof(SendRays));
+        else
+        {
+            Debug.LogWarning("Not running routine anymore!");
+        }
+    }
+
+    IEnumerator SendRays()
+    {
+        for (int i = 0; i < soldiers.Count; i++)
+        {
+            Transform soldierPositions = soldiers[i].transform.GetComponent<Transform>();
+            print($"Soldier(s) {i + 1}: {soldiers[i].transform.position}");
+        }
+
+        if (soldiers.Count == 0) yield break;
+        Debug.LogWarning("Ending loop...");
+    }
+
+    private void UpdateSoldierList()
+    {
+        // Remove any null or destroyed soldiers from the List
+        soldiers.RemoveAll(soldier => soldier == null);
+
+        // find the game object that the soldiers are
+        GameObject[] soldierObjects = GameObject.FindGameObjectsWithTag("AI");
+
+        // iterate over each obj
+        foreach (var obj in soldierObjects)
+        {
+            SoldierUnit soldier = obj.GetComponent<SoldierUnit>();
+
+            if(soldier != null && !soldiers.Contains(soldier))
+                soldiers.Add(soldier);
+        }
+    }
 }
