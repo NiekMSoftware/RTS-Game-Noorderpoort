42c8998cec25eef7bc2d18ffdfee859dd5cb6ba4
Niek Melet <niek+github@meletstudios.com>
2024-01-11 13:59:42 +0100

    Commit 69 of the FOW

--- RTS Project/Assets/Scripts/Misc/FogOfWar.cs
+++ RTS Project/Assets/Scripts/Misc/FogOfWar.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FogOfWar : MonoBehaviour
 {
     // TODO: Refactor the cast performing so it will shoot a ray, then do an overlap sphere on said ground to check the radius
-        // if there are things in that radius show them, else not (this goes for all units).
+    // if there are things in that radius show them, else not (this goes for all units).
 
     // TODO: Rewrite the entire fog of war :(
+
+    [Header("Unit Properties")]
+    public List<SoldierUnit> soldiers;
+    public List<Unit> enemyUnits;
+
+    [Header("Fog of War Properties")]
+    [SerializeField] private Transform fogTransform;
 }
