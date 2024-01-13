9e51fcd5bf2776904b955da7087ae747027a1d58
Niek Melet <niek+github@meletstudios.com>
2024-01-13 18:35:04 +0100

    FieldOfView kan nu enemies DETECTEN

--- RTS Project/Assets/Scripts/Unit/FieldOfView.cs
+++ RTS Project/Assets/Scripts/Unit/FieldOfView.cs
@@ -4,40 +4,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FieldOfView : MonoBehaviour {
-    public float viewRadius;
+    [Range(0, 5)] public float viewRadius;
     [Range(0,360)] public float viewAngle;
 
     public LayerMask targetMask;
     public LayerMask obstacleMask;
 
-    [HideInInspector]
     public List<Transform> visibleTargets = new List<Transform>();
 
     void Start() {
-        StartCoroutine("FindTargetsWithDelay", .2f);
+        StartCoroutine(nameof(FindTargetsWithDelay), .2f);
     }
 
     IEnumerator FindTargetsWithDelay(float delay) {
         while (true) {
             yield return new WaitForSeconds(delay);
-            this.FindVisibleTargets();
+            FindVisibleTargets();
         }
     }
 
     void FindVisibleTargets() {
         visibleTargets.Clear();
         
-        Collider[] targetsInRadius = Physics.OverlapSphere(transform.position, this.viewRadius, this.targetMask);
+        Collider[] targetsInRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
         for (int i = 0; i < targetsInRadius.Length; i++) 
         {
             Transform target = targetsInRadius[i].transform;
             Vector3 dirToTarget = (target.position - transform.position).normalized;
             
-            if (Vector3.Angle(transform.forward, dirToTarget) < this.viewAngle / 2)
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
             {
                 float distanceToTarget = Vector3.Distance(transform.position, target.position);
 
-                if (!Physics.Raycast(transform.position, dirToTarget, distanceToTarget, this.obstacleMask)) {
+                if (!Physics.Raycast(transform.position, dirToTarget, distanceToTarget, obstacleMask)) {
                     // See the target
                     print("Found an enemy");
                     visibleTargets.Add(target);
